c7308fc90a1d3a2133d9c4c2e84e2d761c7ee747
Djordje <djordje.petrovic@net-informatika.com>
2019-09-09 08:33:15 +0200

    Add temporary display method

--- csWeatherApp/Model/CurrentConditionDisplay.cs
+++ csWeatherApp/Model/CurrentConditionDisplay.cs
@@ -4,12 +4,14 @@ using csWeatherApp.WeatherApiModel;
 
 namespace csWeatherApp
 {
-    class CurrentConditionDisplay : IObserver
+    class CurrentConditionDisplay : IObserver, IDisplay
     {
         private Observable _subject;
         private DateTime _dateCalculated;
         private DateTime _sunrise;
         private DateTime _sunset;
+
+        private WeatherData _weatherData;
 
         public CurrentConditionDisplay(Observable subject)
         {
@@ -18,31 +20,11 @@ namespace csWeatherApp
 
         }
         public void Update(object sender, EventArgs args)
-        {
-            // using pull model
-            WeatherData weatherData = sender as WeatherData;
-            if (weatherData != null)
+        {   
+            _weatherData = sender as WeatherData;
+            if (_weatherData != null)
             {
-                _dateCalculated = UnixTimestampConverter.UnixTimestampToDateTime(weatherData.DateCalculated);
-                _sunrise = UnixTimestampConverter.UnixTimestampToDateTime(weatherData.SystemData.Sunrise);
-                _sunset = UnixTimestampConverter.UnixTimestampToDateTime(weatherData.SystemData.Sunset);
-                Console.WriteLine($"Calculated: {_dateCalculated}");
-                Console.WriteLine($"City: {weatherData.CityName} Country {weatherData.SystemData.Country}");
-                foreach (Weather weather in weatherData.WeatherList)
-                {
-                    Console.WriteLine($"{weather.Title} - {weather.Description}");
-                }
-                Console.WriteLine($"Visibility: {weatherData.Visibility}m - Cloudiness: {weatherData.Clouds.Cloudiness}%");
-
-                Console.WriteLine($"Temperature: {weatherData.MainWeatherData.Temperature} Celsius");
-                Console.WriteLine($"Pressure: {weatherData.MainWeatherData.Pressure} hPA");
-                Console.WriteLine($"Humidity: {weatherData.MainWeatherData.Humidity}%");
-                Console.WriteLine($"Min temp: {weatherData.MainWeatherData.MinDailyTemperature} Celsius");
-                Console.WriteLine($"Max temp: {weatherData.MainWeatherData.MaxDailyTemperature} Celsius");
-                Console.WriteLine($"Wind {weatherData.Wind.Speed}m/s {weatherData.Wind.Degree} Degrees");
-                Console.WriteLine($"Sunrise: {_sunrise}");
-                Console.WriteLine($"Sunset: {_sunset}");
-                Console.WriteLine("\n.......................................................\n");
+                Display();
             }
         }
 
@@ -60,6 +42,29 @@ namespace csWeatherApp
             {
                 _subject.NotifyObserversEventHandler -= Update;
             }
-        }       
+        }
+
+        // TODO: Find better solution for this display logic.
+        public void Display()
+        {
+            // Using pull method.
+            Console.WriteLine($"Calculated: {_dateCalculated}");
+            Console.WriteLine($"City: {_weatherData.CityName} Country {_weatherData.SystemData.Country}");
+            foreach (Weather weather in _weatherData.WeatherList)
+            {
+                Console.WriteLine($"{weather.Title} - {weather.Description}");
+            }
+            Console.WriteLine($"Visibility: {_weatherData.Visibility}m - Cloudiness: {_weatherData.Clouds.Cloudiness}%");
+
+            Console.WriteLine($"Temperature: {_weatherData.MainWeatherData.Temperature} Celsius");
+            Console.WriteLine($"Pressure: {_weatherData.MainWeatherData.Pressure} hPA");
+            Console.WriteLine($"Humidity: {_weatherData.MainWeatherData.Humidity}%");
+            Console.WriteLine($"Min temp: {_weatherData.MainWeatherData.MinDailyTemperature} Celsius");
+            Console.WriteLine($"Max temp: {_weatherData.MainWeatherData.MaxDailyTemperature} Celsius");
+            Console.WriteLine($"Wind {_weatherData.Wind.Speed}m/s {_weatherData.Wind.Degree} Degrees");
+            Console.WriteLine($"Sunrise: {_sunrise}");
+            Console.WriteLine($"Sunset: {_sunset}");
+            Console.WriteLine("\n.......................................................\n");
+        }
     }
 }
